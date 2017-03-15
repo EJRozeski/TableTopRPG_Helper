4bdce7ef11ba0f7dd26816fdcd2c8249ecfd15c0
EJRozeski <ejr7394@cs.rit.edu>
2017-03-15 18:34:28 -0400

    added more functionality to create a hero in the Text Interface.

--- ControllerPTUI.java
+++ ControllerPTUI.java
@@ -1,7 +1,33 @@
+import java.util.Scanner;
 
 public class ControllerPTUI {
-	private
+	private PlayerModel model;
+	
+	public static PlayerClass newHero(){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a name for your Hero: ");
+		String name = sc.nextLine();
+		System.out.println("What is your race?: ");
+		String race = sc.nextLine();
+		System.out.println("What is your class?: ");
+		String clas = sc.nextLine();
+		System.out.println("What is your alignment?: ");
+		String align = sc.nextLine();
+		System.out.println("Welcome " + name);
+		PlayerClass res = new PlayerClass(name,race,clas,align);
+		return res;	
+	}
+	
 	public static void main(String[] args){
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Are you a returning Hero? (yes/no)");
+		String hero = sc.nextLine();
+		if(hero.equals("no")){
+			PlayerClass playr = newHero();
+		}
+		while(true){
+			break;
+		}
+		sc.close();
 	}
 }
